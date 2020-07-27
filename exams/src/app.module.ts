0575520b602dd003cf2996078103b89d10a50dd7
Maciej Mycielski <maciejmycielski@gmail.com>
2020-07-27 17:23:23 +0200

    project structure for database connection

--- exams/src/app.module.ts
+++ exams/src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersService } from './users/users.service';
+import { UsersController } from './users/users.controller';
+import { UsersModule } from './users/users.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [MongooseModule.forRoot('mongodb://<dbuser>:<dbpassword>@ds249355.mlab.com:49355/heroku_4tr7dmc2'), UsersModule],
+  controllers: [AppController, UsersController],
+  providers: [AppService, UsersService],
 })
 export class AppModule {}
